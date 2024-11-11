1878fe651744efda5a1e4c4327a846f1e1ab8b9f
afranquez12 <163781509+afranquez12@users.noreply.github.com>
2024-11-11 11:44:57 -0800

    ERR2

---
+++ src/CSC450_CTA1_ERR2.cpp
@@ -0,0 +1,27 @@
+//============================================================================
+// Name        : CSC450_CTA1_ERR2.cpp
+// Author      : Abnel Franquez
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>   // Include correct header for input-output
+using namespace std;   // Standard namespace declaration
+
+// Main Function
+int main()
+{
+    double myMoney = 1000.50; // this should be printed out
+
+    // Standard Output Statement
+    cout << "Please be sure to correct all syntax errors in this program" << endl;
+    cout << "I have corrected all errors for this program." << endl;
+    cout << "The total amount of money available is = " << myMoney << endl;
+
+    // Wait For Output Screen (cross-platform)
+    cin.get();  // waits for the user to press Enter
+
+    // Main Function return Statement
+    return 0;
+}
